6290ea73ed0d81a143b9a4172942e8745b589a2c
SherryShuranHuang <huangsr6@bu.edu>
2015-03-27 19:23:48 -0400

    Socked practice. succeeded

--- JavaPractice/socketProgramming/Client.java
+++ JavaPractice/socketProgramming/Client.java
@@ -7,24 +7,53 @@ public class Client {
 	Socket socket;
 	BufferedReader in;
 	PrintWriter out;
+	
+	public static final String IP_ADDR = "localhost";
+	public static final int PORT = 12345;
+
 	public Client() throws IOException
 	{
+		System.out.println("客户端启动...");    
+        System.out.println("当接收到服务器端字符为 \"OK\" 的时候, 客户端将终止\n");
 		try
 		{
-			socket = new Socket("hostname", 10023);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new PrintWriter(socket.getOutputStream(),true);
-			BufferedReader line = new BufferedReader(new InputStreamReader(System.in));
-			out.println(line.readLine());
-			line.close();
-			out.close();
-			in.close();
-			socket.close();
+			socket = new Socket(IP_ADDR,PORT);
+			if(socket.isConnected()) System.out.println("Connected to the localhost");
+			DataInputStream input = new DataInputStream(socket.getInputStream()); 
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream()); 
+			System.out.print("请输入: \t");    
+            String str = new BufferedReader(new InputStreamReader(System.in)).readLine();    
+            out.writeUTF(str); 
+            
+            String ret = input.readUTF();     
+            System.out.println("服务器端返回过来的是: " + ret);    
+            
+            // 如接收到 "OK" 则断开连接    
+            if ("OK".equals(ret)) {    
+                System.out.println("客户端将关闭连接");    
+                //Thread.sleep(500);    
+                //break;    
+            }else{
+            	System.out.println("客户端连接");  
+            }
+              
+            out.close();  
+            input.close();  
+
 		}
 		catch (UnknownHostException e){
 			System.err.println("Don't know about host: hostname");
 		}catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to: hostname");
+        }finally {  
+            if (socket != null) {  
+                try {  
+                    socket.close();  
+                } catch (IOException e) {  
+                    socket = null;   
+                    System.out.println("客户端 finally 异常:" + e.getMessage());   
+                }  
+            }  
         }
 	}
 	public static void main(String[] args) throws IOException {
--- JavaPractice/socketProgramming/Server.java
+++ JavaPractice/socketProgramming/Server.java
@@ -1,6 +1,9 @@
 package socketProgramming;
 
 import java.io.BufferedReader;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -8,30 +11,41 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	public static final int PORT = 10023;
+	public static final int PORT = 10022;
 	static ServerSocket serverSocket;
+	static Socket client;
 	public static void init() {
 		try{
 			serverSocket = new ServerSocket(PORT);
 			while(true){
-				Socket client = serverSocket.accept();
-				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				PrintWriter out = new PrintWriter(client.getOutputStream(),true);
-				String line = in.readLine();
-				out.println("your input is"+line);
-				out.close();
-				in.close();
-				//client.close();
+				client = serverSocket.accept();
+				DataInputStream input = new DataInputStream(client.getInputStream()); 
+				String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException  
+                // 处理客户端数据    
+                System.out.println("客户端发过来的内容:" + clientInputStr);
+                // 向客户端回复信息    
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());    
+                System.out.print("请输入:\t");    
+                // 发送键盘输入的一行    
+                String s = new BufferedReader(new InputStreamReader(System.in)).readLine();    
+                out.writeUTF(s);    
+                  
+                out.close();    
+                input.close();    
+
 			}
 			
 		}catch(Exception e){
 			System.out.println("Server is not properly acting"+e.getMessage());
 		}finally {    
-			try{
-				serverSocket.close();
-			}catch(Exception e){
-				System.out.println("Server is not properly acting"+e.getMessage());
-			}
+			if (client != null) {    
+                try {    
+                    client.close();    
+                } catch (Exception e) {    
+                	client = null;    
+                    System.out.println("服务端 finally 异常:" + e.getMessage());    
+                }    
+            }   
         }   
 	}
 	
