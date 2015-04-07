b78d72baa333461eed2796d86d856a829ec1e654
SherryShuranHuang <huangsr6@bu.edu>
2015-04-07 16:18:17 -0400

    try2

--- src/EPIC/WellOrderedStr.java
+++ src/EPIC/WellOrderedStr.java
@@ -1,4 +1,9 @@
 package EPIC;
+
+import java.util.ArrayList;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 /*
  * Well-ordered String:
  * You know a password is well-ordered string. Well-ordered string means that the order 
@@ -13,7 +18,28 @@ public class WellOrderedStr {
 			return;
 		}
 		getValidPassword(passLength,0,"");
+		
+//		ArrayList<String> res = new ArrayList<String>();
+//		getValidPassword(passLength,0,"",res);
+//		for(int i = 0;i<res.size();i++){
+//			System.out.println(res.get(i));
+//		}
+		
 	}
+	/**
+	 * If we need to return all the results, use an arraylist to store
+	 */
+//	private void getValidPassword(int n, int start, String item, ArrayList<String> res){
+//		if(n==0){
+//			res.add(item);
+//			return;
+//		}
+//		for(int i=start;i<26;i++){
+//			getValidPassword(n-1, i+1,item+(char)('a'+i));
+//			getValidPassword(n-1, i+1,item+(char)('A'+i));
+//		}
+//	}
+	
 	private void getValidPassword(int n, int start, String pre){
 		if(n==0){
 			System.out.println(pre);
